bc7acccd8d12a3c36d4baec807053826a1a3e0da
thisis-arman <armanhossen1549@gmail.com>
2023-05-19 21:35:16 +0600

    mongodb setup in server site

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -10,6 +11,48 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('PlayTime paradise serverrr');
 });
+
+console.log(process.env.DB_Username)
+
+const uri = `mongodb+srv://${process.env.DB_Username}:${process.env.DB_Password}@cluster0.kwah0lw.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const toyProductCollection = client.db('PlayTimeParadiseDB').collection('toyProduct')
+
+
+    app.get('/toyproduct',async(req,res)=>{
+        const result = await toyProductCollection.find({}).toArray()
+        res.send(result)
+    })
+    app.get('/toyproduct/:id',async(req,res)=>{
+        const id = req.params.id
+        const query = {_id :new ObjectId(id)}
+        const result = await toyProductCollection.findOne(query)
+        res.send(result)
+
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
